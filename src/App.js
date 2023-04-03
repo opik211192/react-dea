60ebed4d73a623cf741a9426154766f5a54b9a6d
opik211192 <opiktaofik21@gmail.com>
2023-04-03 14:54:52 +0700

    pemanasam

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
+  const [myName, setMyName] = useState("ibenk");
+  const [myAge, setMyAge] = useState(25);
+
+  const handleClick = () => {
+    // if (myName === "ibenk") {
+    //   setMyName("pratama");
+    // } else {
+    //   setMyName("ibenk");
+    // }
+
+    myName === "pratama" ? setMyName("ibenk") : setMyName("pratama");
+    //setMyName("Dea Afrizal");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{myName}</p>
+        <button onClick={handleClick} className="mt-5">
+          {myName === "pratama" ? "Balikin Nama" : "Ganti nama"}
+        </button>
+
+        <div className="d-flex mt-3">
+          <button onClick={() => setMyAge((prev) => prev + 1)} className="me-5">
+            +
+          </button>
+          <p>{myAge}</p>
+          <button onClick={() => setMyAge((prev) => prev - 1)} className="ms-5">
+            -
+          </button>
+        </div>
       </header>
     </div>
   );
