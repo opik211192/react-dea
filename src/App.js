49c6d1e7f7286b81b4ad00ced785e74344392c3b
opik211192 <opiktaofik21@gmail.com>
2023-04-05 10:54:58 +0700

    ambli text input, add todolist & delete todolist

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import "./App.css";
 import {
   Container,
   Form,
@@ -9,9 +9,31 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
-import "./App.css";
+import { FaPlusCircle } from "react-icons/fa";
 
 function App() {
+  const [todoName, setTodoName] = useState("");
+  const [todolist, setTodolist] = useState([]);
+
+  //2.tambah todo
+  const addTodo = () => {
+    const dataTodo = { todoName, status: false };
+    setTodolist([dataTodo, ...todolist]);
+    setTodoName("");
+  };
+
+  //3. delete todo
+  const deleteTodo = (index) => {
+    // console.log(index);
+    setTodolist(todolist.filter((value, idx) => index !== idx));
+  };
+
+  //1. tangkap dlu text inputnya
+  const handleChangeName = (e) => {
+    const data = e.target.value;
+    setTodoName(data);
+  };
+
   return (
     <Container>
       <h1 className="text-center mt-5">Todo Web App</h1>
@@ -20,26 +42,46 @@ function App() {
       <Card className="mt-5">
         <Card.Body>
           <Stack direction="horizontal" gap={3}>
-            <Form.Control type="text" placeholder="Masukan todo..." />
-            <Button>Tambah</Button>
+            <Form.Control
+              type="text"
+              placeholder="Masukan todo..."
+              onChange={handleChangeName}
+              value={todoName}
+            />
+            <Button onClick={addTodo}>
+              <FaPlusCircle />
+            </Button>
           </Stack>
         </Card.Body>
       </Card>
 
       {/* List todo */}
-      <Card className="mt-5">
-        <Card.Body>
-          <Row>
-            <Col>
-              <h3>Nama Todo</h3>
-            </Col>
+      <div className="mt-5">
+        {todolist.map((value, index) => {
+          return (
+            <Card className="mt-3">
+              <Card.Body>
+                <Row>
+                  <Col>
+                    <h3>{value.todoName}</h3>
+                  </Col>
 
-            <Col xl="1">
-              <Button>Selesai</Button>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+                  <Col xl="1">
+                    <Button>Selesai</Button>
+                  </Col>
+                </Row>
+                <Button
+                  variant="danger"
+                  className="mt-2"
+                  onClick={() => deleteTodo(index)}
+                >
+                  Hapus
+                </Button>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
     </Container>
   );
 }
