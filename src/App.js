c5e3350774913e625d6b33384c4b7d9403b2af5a
opik211192 <opiktaofik21@gmail.com>
2023-04-05 08:28:24 +0700

    persiapan di ui todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+
+import {
+  Container,
+  Form,
+  Button,
+  Stack,
+  Card,
+  Row,
+  Col,
+} from "react-bootstrap";
 import "./App.css";
 
 function App() {
   return (
     <Container>
       <h1 className="text-center mt-5">Todo Web App</h1>
+
+      {/* input todo */}
+      <Card className="mt-5">
+        <Card.Body>
+          <Stack direction="horizontal" gap={3}>
+            <Form.Control type="text" placeholder="Masukan todo..." />
+            <Button>Tambah</Button>
+          </Stack>
+        </Card.Body>
+      </Card>
+
+      {/* List todo */}
+      <Card className="mt-5">
+        <Card.Body>
+          <Row>
+            <Col>
+              <h3>Nama Todo</h3>
+            </Col>
+
+            <Col xl="1">
+              <Button>Selesai</Button>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
     </Container>
   );
 }
